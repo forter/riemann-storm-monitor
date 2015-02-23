6b90548174d97d572bb3002eb6429b5ecce8cfd0
Re'em Bensimhon <reem@forter.com>
2015-02-23 19:28:25 +0200

    Few chenges to support new TopologyIT behavior

--- src/main/java/com/forter/monitoring/events/ExceptionEvent.java
+++ src/main/java/com/forter/monitoring/events/ExceptionEvent.java
@@ -1,5 +1,6 @@
 package com.forter.monitoring.events;
 
+import com.forter.monitoring.utils.MonitoringConstants;
 import com.google.common.base.Throwables;
 
 public class ExceptionEvent extends RiemannEvent {
@@ -10,6 +11,8 @@ public class ExceptionEvent extends RiemannEvent {
     }
 
     public ExceptionEvent(Throwable t) {
-        this(Throwables.getStackTraceAsString(t));
+        this(t.getMessage());
+        this.attribute(MonitoringConstants.ERROR_STACK_ATTR_ID, Throwables.getStackTraceAsString(t));
+        this.attribute(MonitoringConstants.ERROR_TYPE_ATTR_ID, t.getClass().getSimpleName());
     }
 }
