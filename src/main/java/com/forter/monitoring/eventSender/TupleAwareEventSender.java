0aaf6ac86ad8c3aa762fefbed280f714959339b4
anya <anya@forter.com>
2015-07-20 18:06:39 +0300

    Add TupleAwareEventSender for sending events in contract bolts

---
+++ src/main/java/com/forter/monitoring/eventSender/TupleAwareEventSender.java
@@ -0,0 +1,21 @@
+package com.forter.monitoring.eventSender;
+
+import backtype.storm.tuple.Tuple;
+import com.forter.monitoring.events.RiemannEvent;
+import lombok.Setter;
+
+public class TupleAwareEventSender implements EventSender{
+    private EventSender delegate;
+    @Setter
+    private Tuple currentTuple;
+
+    public TupleAwareEventSender(EventSender delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public void send(RiemannEvent event) {
+        event.tuple(currentTuple);
+        delegate.send(event);
+    }
+}
