4957ba729a43dffa404dadd38cc00c8c7fe00e9c
Yacov Buchritz <yacov.buchritz@forter.com>
2019-04-30 11:29:49 +0300

    overload exceptionEvent with bool input

--- src/main/java/com/forter/monitoring/events/ExceptionEvent.java
+++ src/main/java/com/forter/monitoring/events/ExceptionEvent.java
@@ -4,14 +4,31 @@ import com.forter.monitoring.utils.MonitoringConstants;
 import com.google.common.base.Throwables;
 
 public class ExceptionEvent extends RiemannEvent {
+
     public ExceptionEvent(String exMessage) {
         super();
         this.description(exMessage);
         this.tags.add("exception");
     }
 
+    public ExceptionEvent(String exMessage, boolean isRatioException) {
+        super();
+        this.description(exMessage);
+        if (isRatioException) {
+            this.tags.add("exception");
+        } else {
+            this.tags.add("ratio-exception");
+        }
+    }
+
     public ExceptionEvent(Throwable t) {
         this(t.getMessage());
+        this.attribute(MonitoringConstants.ERROR_STACK_ATTR_ID, Throwables.getStackTraceAsString(t));
+        this.attribute(MonitoringConstants.ERROR_TYPE_ATTR_ID, t.getClass().getSimpleName());
+    }
+
+    public ExceptionEvent(Throwable t, boolean isRatioException) {
+        this(t.getMessage(), isRatioException);
         this.attribute(MonitoringConstants.ERROR_STACK_ATTR_ID, Throwables.getStackTraceAsString(t));
         this.attribute(MonitoringConstants.ERROR_TYPE_ATTR_ID, t.getClass().getSimpleName());
     }
